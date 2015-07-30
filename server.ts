148d4f14ed801cceefaf33ad8caa695e96af2789
Carlos Martinez <c.martinez@esciencecenter.nl>
2015-07-30 11:05:23 +0200

    Bengaluru project file

--- server.ts
+++ server.ts
@@ -1,31 +1,30 @@
-require('rootpath')();
-﻿import express              = require('express');
-import http                 = require('http');
-import path                 = require('path');
-//import offlineSearch        = require('cs-offline-search');
+require("rootpath")();
+﻿import express              = require("express");
+import http                 = require("http");
+import path                 = require("path");
+//import offlineSearch        = require("cs-offline-search");
 import cc                   = require("ServerComponents/dynamic/ClientConnection");
-import creator              = require('ServerComponents/creator/MapLayerFactory');
+import creator              = require("ServerComponents/creator/MapLayerFactory");
 import DataSource           = require("ServerComponents/dynamic/DataSource");
-import MessageBus           = require('ServerComponents/bus/MessageBus');
-import BagDatabase          = require('ServerComponents/database/BagDatabase');
-import ConfigurationService = require('ServerComponents/configuration/ConfigurationService');
+import MessageBus           = require("ServerComponents/bus/MessageBus");
+import BagDatabase          = require("ServerComponents/database/BagDatabase");
+import ConfigurationService = require("ServerComponents/configuration/ConfigurationService");
 import DynamicProject       = require("ServerComponents/dynamic/DynamicProject");
 
 // setup socket.io object
-var favicon    = require('serve-favicon');
-var bodyParser = require('body-parser')
+var favicon    = require("serve-favicon");
+var bodyParser = require("body-parser");
 var server     = express();
 
-var httpServer = require('http').Server(server);
+var httpServer = require("http").Server(server);
 var cm         = new cc.ConnectionManager(httpServer);
 var messageBus = new MessageBus.MessageBusService();
-var config     = new ConfigurationService('./configuration.json');
-
+var config     = new ConfigurationService("./configuration.json");
 
 // all environments
 var port = "3002";
-server.set('port', port);
-server.use(favicon(__dirname + '/public/favicon.ico'));
+server.set("port", port);
+server.use(favicon(__dirname + "/public/favicon.ico"));
 server.use(bodyParser.json()); // support json encoded bodies
 server.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
@@ -40,11 +39,11 @@ server.get("/datasource", ds.getDataSource);
 
 var bagDatabase = new BagDatabase(config);
 var mapLayerFactory = new creator.MapLayerFactory(bagDatabase, messageBus);
-server.post('/projecttemplate', (req, res) => mapLayerFactory.process(req, res));
+server.post("/projecttemplate", (req, res) => mapLayerFactory.process(req, res));
 
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, "public")));
 console.log("started");
 
-httpServer.listen(server.get('port'),() => {
-    console.log('Express server listening on port ' + server.get('port'));
+httpServer.listen(server.get("port"),() => {
+    console.log("Express server listening on port " + server.get("port"));
 });
